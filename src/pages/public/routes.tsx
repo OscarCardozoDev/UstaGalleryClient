34e7711c5f7ac6ae512a074e386de73df20f4ee0
OscarCardozoDev <oscar.cardozo30@hotmail.com>
2026-04-26 18:41:19 -0500

    feat(routes): scaffold ArtistPage at /artist/:uid

--- src/pages/public/routes.tsx
+++ src/pages/public/routes.tsx
@@ -4,40 +4,27 @@ import GalleryPage from "./modules/Gallery/Gallery";
 import ShowImagePage from "./modules/ShowImage/ShowImage";
 import Events from "./modules/Events/Events";
 import EventDetail from "./modules/EventDetail/EventDetail";
+import ArtistPage from "./modules/ArtistPage/ArtistPage";
 
 export default function MainPageRoutes() {
   return (
     <Routes>
       <Route index element={<WelcomePage />} />
-      <Route path="/gallery" element={<GalleryPage/>} />
-      <Route path="/show-picture/:uid" element={<ShowImagePageWrapper/>} />
-      <Route path="/events" element={<Events/>} />
-      <Route path="/events/:uid" element={<EventDetail/>} />
-
-      {/*
-        <Hero 
-          widthHeader={widthButton} 
-          heightHeader={heightHeader}
-          heightCarrusel={300} 
-          widthCarrusel={400} 
-          positionYCarrusel={525} 
-        />
-      */}
+      <Route path="/gallery" element={<GalleryPage />} />
+      <Route path="/show-picture/:uid" element={<ShowImagePageWrapper />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/events/:uid" element={<EventDetail />} />
+      <Route path="/artist/:uid" element={<ArtistPageWrapper />} />
     </Routes>
   );
 }
 
-// -------------------------- Wrappers -------------------------- //
-
-/**
- * ShowImagePageWrapper
- * 
- * Este wrapper es necesario para pasar el parámetro `uid` a ShowImagePage.
- * Al envolver ShowImagePage en este componente, se asegura de que se renderice
- * con el parámetro `uid` correcto, evitando problemas de rendimiento y
- * asegurando que cada imagen se muestre correctamente.
- */
 function ShowImagePageWrapper() {
   const { uid } = useParams<{ uid: string }>();
   return <ShowImagePage key={uid} />;
+}
+
+function ArtistPageWrapper() {
+  const { uid } = useParams<{ uid: string }>();
+  return <ArtistPage key={uid} />;
 }
